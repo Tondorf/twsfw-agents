b9d5fd5e0e0925440b3faeb8a7c268ebc6608c24
Lennart Buhl <git@hackmate.de>
2025-03-01 23:34:19 +0100

    go BUILDS

--- goxample/goxample.go
+++ goxample/goxample.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+//#include "../example/twsfw_agent.h"
+import "C"
+
+//go:wasm-module goxample
+//export twsfw_agent_act
+func twsfw_agent_act(agents *C.struct_twsfw_agent, n_agents uint32,
+	missiles *C.struct_twsfw_missile, n_missiles uint32,
+	world *C.struct_twsfw_world, id int32,
+	action *int32) float32 {
+	return 10
+}
+
 func main() {
-    fmt.Println("Hello, WebAssembly!")
+	fmt.Println("Hello, WebAssembly!")
 }
